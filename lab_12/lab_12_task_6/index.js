0dd3d1dbaf5b2a92d05974428251aea9d201b7c4
Bohdan Serdiuk <bohdan.serdiuk22@gmail.com>
2026-02-01 16:45:18 +0200

    Finish lab 12

---
+++ lab_12/lab_12_task_6/index.js
@@ -0,0 +1,17 @@
+let firstName = 'Bohdan';
+let surname = 'Serdiuk';
+let studyGroup = 'KN-321';
+let birthYear = 2008;
+let isMarried = false;
+
+console.log(typeof birthYear + ' ' + birthYear);
+console.log(typeof isMarried + ' ' + isMarried);
+console.log(typeof firstName + ' ' + firstName);
+console.log(typeof surname + ' ' + surname);
+console.log(typeof studyGroup + ' ' + studyGroup);
+
+let nullVar = null;
+let undefVar = undefined;
+
+console.log(typeof nullVar);
+console.log(typeof undefVar);
